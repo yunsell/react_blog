c2a3a6842814de1047246c36b77a97e9b6a7f264
yunsell <yunsell31@gmail.com>
2022-01-18 23:48:05 +0900

    3강 완료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [a,b] = useState('남자 코트 추천');
+
+  let posts = '강남 고기 맛집';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <div className='list'>
+        <h3> { a } </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
