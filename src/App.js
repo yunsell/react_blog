54a2d86fe4e3cbd8ac89ce7d63147ef5e21a088b
yunsell <yunsell31@gmail.com>
2022-01-21 01:05:47 +0900

    React 5강 완료

--- src/App.js
+++ src/App.js
@@ -4,18 +4,38 @@ import './App.css';
 
 function App() {
 
-  let [a,b] = useState('남자 코트 추천');
-
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 맛집', '이쁜 옷가게']);
+  let [따봉, 따봉변경] = useState(0);
   let posts = '강남 고기 맛집';
+
+  function 제목바꾸기() {
+    let newArray = [...글제목].sort();
+    // newArray[0] = '여자 코트 추천';
+    글제목변경(newArray);
+  }
+  
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
+      <button onClick={ 제목바꾸기 }>버튼</button>
       <div className='list'>
-        <h3> { a } </h3>
+        <h3> { 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1) } }>👍</span> {따봉} </h3>
         <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+
+      <div className='list'>
+        <h3> { 글제목[1] } </h3>
+        <p>2월 18일 발행</p>
+        <hr/>
+      </div>
+
+      <div className='list'>
+        <h3> { 글제목[2] } </h3>
+        <p>2월 19일 발행</p>
         <hr/>
       </div>
     </div>
